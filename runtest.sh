1a6f3942689eeba5fb726c53b326f93b98034feb
Maia CHARDIN <mchardin@e2r11p3.42.fr>
2019-11-02 20:58:23 +0100

    More tests, still not UTD

--- runtest.sh
+++ runtest.sh
@@ -1,7 +1,8 @@
 rm results/* diff.txt &> /dev/null
 cp ../*.h includes/ &> /dev/null
 cp ../*/*.h includes/ &> /dev/null
-gcc srcs/main.c -w -D PRINT="printf" -I ./includes -o printf.out >> /dev/null
+touch results/expected_return.txt results/test_return.txt
+gcc srcs/main.c ../libft/ft_putnbr_fd.c ../libft/ft_putchar_fd.c -D PRINT="printf" -D REAL_F=1 -I ./includes -o printf.out >> /dev/null
 ./printf.out >> results/expected_result.txt
 make -C srcs/ re > /dev/null 2> results/compilerror.txt
 ./srcs/tester.out > results/test_result.txt #2> results/error.txt
@@ -11,9 +12,11 @@ sed -n ${i}p results/expected_result.txt >> printf.txt
 
 while [ -s printf.txt ]
 do
-	rm -f printf.txt ft.txt
+	rm -f printf.txt ft.txt printf_r.txt ft_r.txt
 	sed -n ${i}p results/expected_result.txt >> printf.txt
 	sed -n ${i}p results/test_result.txt >> ft.txt
+	sed -n ${i}p results/expected_return.txt >> printf_r.txt
+	sed -n ${i}p results/test_return.txt >> ft_r.txt
 	if [ $i -lt 10 ] ; then
 		echo -n "Test   $i :" 
 	elif [ $i -lt 100 ] ; then
@@ -22,9 +25,14 @@ do
 		echo -n "Test $i :"
 	fi
 	if diff ft.txt printf.txt >> diff.txt ; then
- 		echo -ne "\033[0;32m[OK]	\033[0m" && echo "No Diff" >> diff.txt
+ 		echo -ne "\033[0;32m[OK]\033[0m" && echo "No Diff" >> diff.txt
 	else
- 		 echo -ne "\033[0;31m[KO]	\033[0m"
+ 		 echo -ne "\033[0;31m[KO]\033[0m"
+	fi
+	if diff ft_r.txt printf_r.txt >> diff.txt ; then
+ 		echo -ne "\033[0;32m [OK]	\033[0m" && echo "No Diff" >> diff.txt
+	else
+ 		 echo -ne "\033[0;31m [KO]	\033[0m"
 	fi
 	let "j = $i % 10"
 	if [ $j -eq 0 ] ; then
@@ -33,5 +41,5 @@ do
 	let "i += 1"
 done
 echo
-rm includes/* printf.txt ft.txt printf.out &> /dev/null
+rm includes/* *.txt printf.out &> /dev/null
 make -C srcs/ fclean > /dev/null 2> results/compilerror.txt
--- srcs/main.c
+++ srcs/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include "ft_printf.h"
 
 int main()
 {
-	int		a = 0;
+	int		a = -4;
+	int		b;
 	int		i = 8;
 	int		j = -12;
 	int		k = 123456789;
@@ -12,21 +14,148 @@ int main()
 	int		m = -12345678;
 	char	*n = "abcdefghijklmnop";
 	char	c = 'a';
-	
-	PRINT("Simple input test\n"); //T1
-	PRINT("%i, %d, %d, %d, %d, %s, %c, %d\n", i, j, k, l, m, n, c, c); //T2
-	PRINT("%0i, %0d, %0d, %0d, %0d, %0s, %0c, %0d\n", i, j, k, l, m, n, c, c); //T3
-	while(a < 8)
+	int		ret[1000];
+	int		count = -1;
+	int		fd;
+
+	ret[++count] = PRINT("Simple input test"); //T1
+	PRINT("\n");
+	ret[++count] = PRINT("%i, %d, %d, %d, %d, %s, %c, %d", i, j, k, l, m, n, c, c); //T2
+	PRINT("\n");
+	ret[++count] = PRINT("%0i, %0d, %0d, %0d, %0d, %0d", i, j, k, l, m, c); //T3
+	PRINT("\n");
+	ret[++count] = PRINT("%1i, %1d, %1d, %1d, %1d, %1d", i, j, k, l, m, c); //T4
+	PRINT("\n");
+	ret[++count] = PRINT("%2i, %2d, %2d, %2d, %2d, %2d", i, j, k, l, m, c); //T5
+	PRINT("\n");
+	ret[++count] = PRINT("%3i, %3d, %3d, %3d, %3d, %3d", i, j, k, l, m, c); //T6
+	PRINT("\n");
+	ret[++count] = PRINT("%4i, %4d, %4d, %4d, %4d, %4d", i, j, k, l, m, c); //T7
+	PRINT("\n");
+	ret[++count] = PRINT("%5i, %5d, %5d, %5d, %5d, %5d", i, j, k, l, m, c); //T8
+	PRINT("\n");
+	ret[++count] = PRINT("%6i, %6d, %6d, %6d, %6d, %6d", i, j, k, l, m, c); //T9
+	PRINT("\n");
+	ret[++count] = PRINT("%7i, %7d, %7d, %7d, %7d, %7d", i, j, k, l, m, c); //T10
+	PRINT("\n");
+	ret[++count] = PRINT("%8i, %8d, %8d, %8d, %8d, %8d", i, j, k, l, m, c); //T11
+	PRINT("\n");
+	ret[++count] = PRINT("%0.i, %0.d, %0.d, %0.d, %0.d, %0.d", i, j, k, l, m, c); //T12
+	PRINT("\n");
+	ret[++count] = PRINT("%.0i, %.0d, %.0d, %.0d, %.0d, %.0d", i, j, k, l, m, c); //T13
+	PRINT("\n");
+	ret[++count] = PRINT("%0.0i, %0.0d, %0.0d, %0.0d, %0.0d, %0.0d", i, j, k, l, m, c); //T14
+	PRINT("\n");
+	ret[++count] = PRINT("%-0i, %-0d, %-0d, %-0d, %-0d, %-0d", i, j, k, l, m, c); //T15
+	PRINT("\n");
+	ret[++count] = PRINT("%0-i, %0-d, %0-d, %0-d, %0-d, %0-d", i, j, k, l, m, c); //T16
+	PRINT("\n");
+	ret[++count] = PRINT("%-.i, %-.d, %-.d, %-.d, %-.d, %-.d", i, j, k, l, m, c); //T17
+	PRINT("\n");
+	ret[++count] = PRINT("%-.0i, %-.0d, %-.0d, %-.0d, %-.0d, %-.0d", i, j, k, l, m, c); //T18
+	PRINT("\n");
+	ret[++count] = PRINT("%-i, %-d, %-d, %-d, %-d, %-d", i, j, k, l, m, c); //T19
+	PRINT("\n");
+	ret[++count] = PRINT("%.i, %.d, %.d, %.d, %.d, %.d", i, j, k, l, m, c); //T20
+	PRINT("\n");
+	ret[++count] = PRINT("%-0.0i, %-0.0d, %-0.0d, %-0.0d, %-0.0d, %-0.0d", i, j, k, l, m, c); //T21
+	PRINT("\n");
+	ret[++count] = PRINT("%00i, %00d, %00d, %00d, %00d, %00d", i, j, k, l, m, c); //T22
+	PRINT("\n");
+	ret[++count] = PRINT("%1.i, %1.d, %1.d, %1.d, %1.d, %1.d", i, j, k, l, m, c); //T23
+	PRINT("\n");
+	ret[++count] = PRINT("%.1i, %.1d, %.1d, %.1d, %.1d, %.1d", i, j, k, l, m, c); //T24
+	PRINT("\n");
+	ret[++count] = PRINT("%1.1i, %1.1d, %1.1d, %1.1d, %1.1d, %1.1d", i, j, k, l, m, c); //T25
+	PRINT("\n");
+	ret[++count] = PRINT("%-1i, %-1d, %-1d, %-1d, %-1d, %-1d", i, j, k, l, m, c); //T26
+	PRINT("\n");
+	ret[++count] = PRINT("%-01i, %-01d, %-01d, %-01d, %-01d, %-01d", i, j, k, l, m, c); //T27
+	PRINT("\n");
+	ret[++count] = PRINT("%-1.i, %-1.d, %-1.d, %-1.d, %-1.d, %-1.d", i, j, k, l, m, c); //T28
+	PRINT("\n");
+	ret[++count] = PRINT("%-.1i, %-.1d, %-.1d, %-.1d, %-.1d, %-.1d", i, j, k, l, m, c); //T29
+	PRINT("\n");
+	ret[++count] = PRINT("%-0.1i, %-0.1d, %-0.1d, %-0.1d, %-0.1d, %-0.1d", i, j, k, l, m, c); //T30
+	PRINT("\n");
+	ret[++count] = PRINT("%-1.0i, %-1.0d, %-1.0d, %-1.0d, %-1.0d, %-1.0d", i, j, k, l, m, c); //T31
+	PRINT("\n");
+	ret[++count] = PRINT("%-1.1i, %-1.1d, %-1.1d, %-1.1d, %-1.1d, %-1.1d", i, j, k, l, m, c); //T32
+	PRINT("\n");
+	ret[++count] = PRINT("%1.0i, %1.0d, %1.0d, %1.0d, %1.0d, %1.0d", i, j, k, l, m, c); //T33
+	PRINT("\n");
+	while (a < 5) //T34-69
 	{
-		PRINT("%*i, %*d, %*d, %*d, %*d, %*d\n", a, i, a, j, a, k, a, l, a, m, a, c); //T4, 8, 12, 16, 20, 24, 28, 32
-		PRINT("%0*i, %0*d, %0*d, %0*d, %0*d, %0*d\n", a, i, a, j, a, k, a, l, a, m, a, c); //T5, 9, 13, 21, 25, 29, 33
-		PRINT("%-*i, %-*d, %-*d, %-*d, %-*d, %-*d\n", a, i, a, j, a, k, a, l, a, m, a, c); //T6, 10, 14, 22, 26, 30, 34
-		PRINT("%.*i, %.*d, %.*d, %.*d, %.*d, %.*d\n", a, i, a, j, a, k, a, l, a, m, a, c); //T7, 11, 15, 23, 27, 31, 35
+		ret[++count] = PRINT("%*i, %*d, %*d, %*d, %*d, %*d", a, i, a, j, a, k, a, l, a, m, a, c);
+		PRINT("\n");
+		ret[++count] = PRINT("%0*i, %0*d, %0*d, %0*d, %0*d, %0*d", a, i, a, j, a, k, a, l, a, m, a, c);
+		PRINT("\n");
+		ret[++count] = PRINT("%-*i, %-*d, %-*d, %-*d, %-*d, %-*d", a, i, a, j, a, k, a, l, a, m, a, c);
+		PRINT("\n");
+		ret[++count] = PRINT("%.*i, %.*d, %.*d, %.*d, %.*d, %.*d", a, i, a, j, a, k, a, l, a, m, a, c);
+		PRINT("\n");
 		a++;
 	}
-	PRINT("%%, \%, %, \t, \\, \", \', \+, +\n", i); //T36
-	PRINT("%5999999999999999990i, %5999999999999999990d, %5999999999999999990d, %5999999999999999990d, %5999999999999999990d, %5999999999999999990s, %5999999999999999990c, %5999999999999999990d\n", i, j, k, l, m, n, c, c);
+	a = -4;
+	while(a < 5) //T70-393
+	{
+		b = -4;
+		while (b < 5)
+		{
+			ret[++count] = PRINT("%*.*i, %*.*d, %*.*d, %*.*d, %*.*d, %*.*d", a, b, i, a, b, j, a, b, k, a, b, l, a, b, m, a, b, c);
+			PRINT("\n");
+			ret[++count] = PRINT("%-*.*i, %-*.*d, %-*.*d, %-*.*d, %-*.*d, %-*.*d", a, b, i, a, b, j, a, b, k, a, b, l, a, b, m, a, b, c);
+			PRINT("\n");
+			ret[++count] = PRINT("%0*.*i, %0*.*d, %0*.*d, %0*.*d, %0*.*d, %0*.*d", a, b, i, a, b, j, a, b, k, a, b, l, a, b, m, a, b, c);
+			PRINT("\n");
+			ret[++count] = PRINT("%-0*.*i, %-0*.*d, %-0*.*d, %-0*.*d, %-0*.*d, %-0*.*d", a, b, i, a, b, j, a, b, k, a, b, l, a, b, m, a, b, c);
+			PRINT("\n");
+			b++;
+		}
+		a++;
+	}
 
+	// ret[++count] = PRINT("%%, \%, %, \t, \\, \", \', \+, +", i);
+	// // PRINT("\n");
+	// ret[++count] = PRINT("%5i, %5d, %5d, %5d, %5d, %5d", i, j, k, l, m, c);
+	// // PRINT("\n");
+	// ret[++count] = PRINT("%*.i, %.**d, %88.*d, %*.*d, %.*8d, %.8*d", a, i, a, a, j, a, k, a, l, a, m, a, c);
+	// // PRINT("\n");
+	// ret[++count] = PRINT("%-00000-----*i, %-.0*d, %---.--*d, %-----.*d, %0000000000.000*d, %...00*d", a, i, a, j, a, k, a, l, a, m, a, c);
+	// // PRINT("\n");
+	// ret[++count] = PRINT("%.-.-.-.-*i, %---0.*d, %0-0-0-0-0.*d, %-0-0-0-0-.*d, %....*d, %.*.d", a, i, a, j, a, k, a, l, a, m, a, c);
+	// // PRINT("\n");
+	// ret[++count] = PRINT("%599i, %599d, %599d, %599d, %599d, %599s, %599c, %599d", i, j, k, l, m, n, c, c);
+	// PRINT("\n");
+	ret[++count] = PRINT("");
 
-	PRINT("");
+	count = -1;
+	if (REAL_F == 1)
+	{
+		if((fd = open("results/expected_return.txt", O_WRONLY)) == -1)
+		{
+			PRINT("TESTER Error\n\n");
+			return (0);
+		}
+		while (++count < 1000)
+		{
+			ft_putnbr_fd(ret[count], fd);
+			ft_putchar_fd('\n', fd);
+		}
+		close(fd);
+	}
+	else
+	{
+		if((fd = open("results/test_return.txt", O_WRONLY)) == -1)
+		{
+			PRINT("TESTER Error\n\n");
+			return (0);
+		}
+		while (++count < 1000)
+		{
+			ft_putnbr_fd(ret[count], fd);
+			ft_putchar_fd('\n', fd);
+		}
+		close(fd);
+	}
 }
