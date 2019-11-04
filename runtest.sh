339fbae561e6a8c1ad3e26b1f74436df7e77d00d
Maia CHARDIN <mchardin@e2r11p3.42.fr>
2019-11-04 18:05:08 +0100

    compil test added

--- runtest.sh
+++ runtest.sh
@@ -15,8 +15,7 @@ echo "==========================================================================
 echo ""
 i=1
 sed -n ${i}p results/expected_result.txt >> printf.txt
-sed -n ${i}p results/test_result.txt >> ft.txt
-if [ -s ft.txt ] ; then
+if [ -s results/test_result.txt ] ; then
 while [ -s printf.txt ]
 do
 	rm -f printf.txt ft.txt printf_r.txt ft_r.txt
@@ -70,10 +69,12 @@ if diff results/expected_return.txt results/test_return.txt &> /dev/null ; then
  		echo -ne "Return values:\033[0;31m [KO]\033[0m"
 	fi
 echo
+echo
+echo
 
 else
-	echo -e "\033[0;31m _                    _\033[0m"
-	echo -nee "\033[4;31m/!\\"
+	echo -e "\033[0;31m \xEA\x93\xA5                    \xEA\x93\xA5 \033[0m"
+	echo -ne "\033[4;31m/!\\"
 	echo -ne "\033[0m\033[0;31m Does Not Compile \033[0m"
 	echo -e "\033[4;31m/!\\"
 	echo
